53a13d719574207997234e03b492d9213ad73ca3
ntttrung <trung.ntt1210@gmail.com>
2020-09-24 13:18:52 +0700

    done

--- L1/quicksort.c
+++ L1/quicksort.c
@@ -1,35 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX_SIZE 20
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-void quicksort_2_way(int a[], int l, int r);
-void exch(int a[], int i, int j);
-void quicksort_3_way(int a[], int l, int r);
-int *createArray(int size);
-int *dumpArray(int *a, int size);
-void output(int *a, int size);
+#define MAX_NUMBER 1000
+
+void exch(int a[], int i , int j);
+int* createArray(int n);
+int* dumpArray(int* a, int n);
+void output(int x[], int n);
+void quicksort_2_way(int a[], int l, int r) ;
+void quicksort_3_way(int a[], int l, int r) ;
+
+int main()
+{
+	time_t start, end;
+	int *a1,*a2;
+	srand(time NULL);
+	a1=createArray(MAX_NUMBER);
+	a2= dumpArray(a1, MAX_NUMBER);
+	
+	printf("Number of integer:%d\n", MAX_NUMBER);
+	start= clock();
+    quicksort_2_way(a1, 0 , MAX_NUMBER -1);
+	end = clock();
+	//output(a1,MAX_NUMBER);
+	printf("Run in %.8f second(s)\n", difftime(end, start)/CLOCKS_PER_SEC);
+
+	start= clock();
+    quicksort_3_way(a1, 0 , MAX_NUMBER -1);
+	end = clock();
+	//output(a2,MAX_NUMBER);
+	printf("Run in %.8f second(s)", difftime(end, start)/CLOCKS_PER_SEC);
+
+	
 
 
-void quicksort_2_way(int a[], int l, int r) {
-    if (r <= l) return;
-    int i = l, j = r + 1;
-    int v = a[l];
-    while (1) {
-        while (a[++i] < v) if (i == r) break;
-        while (v < a[--j]) if (j == l) break;
-        if (i >= j) break;
-        exch(a, i, j);
+
+}
+
+int* createArray(int n)
+{
+	
+	int *p= (int*)malloc(sizeof(int)*n);
+	int i;
+	for(i=0; i<n ; i++)
+	
+		*(p+i) = rand() % 10;
+	
+	return p;
+}
+
+int * dumpArray(int *a, int size) 
+{
+	int i;
+   int *b= (int*)malloc(sizeof(int)*size);
+    for(i=0; i<size; i++)
+    {
+        *(b+i)=*(a+i);
     }
-    exch(a, l, j);
-    quicksort_2_way(a, l, j - 1);
-    quicksort_2_way(a, j + 1, r);
+    return b;
+    
+}
+void output(int x[], int n)
+{
+	int i;
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",x[i]);	
+	}
+	printf("\n");
 }
 
-void exch(int a[], int i, int j) {
-    int temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+void quicksort_2_way(int a[], int l, int r) 
+{
+	if(r<=l) return;
+	int i=l, j=r+1;
+	int v=a[l];
+	while(1)
+	{
+		while(a[++i]<v) if(i==r) break;
+		while(a[--j]>v) if(j==l) break;
+		if(i>=j) break;
+		exch(a,i,j);
+	}
+	exch(a,l,j);
+	quicksort_2_way(a,l,j-1);
+	quicksort_2_way(a,j+1,r);
 }
+
+void exch(int a[], int i , int j)
+{
+	int temp=a[i];
+	a[i]= a[j];
+	a[j]= temp;
+}
+
 
 void quicksort_3_way(int a[], int l, int r) {
     if (r <= l) return;
@@ -43,35 +108,3 @@ void quicksort_3_way(int a[], int l, int r) {
     quicksort_3_way(a, l, p - 1);
     quicksort_3_way(a, q + 1, r);
 }
-
-int *createArray(int size) {
-    int i;
-    int *a= malloc(sizeof(size));  
-    for (i= 0; i < size; i++) {
-        *(a + i) = rand() % 10;
-    }
-    return a;
-}
-
-int *dumpArray(int *a, int size) {
-    int *b= malloc(sizeof(size));
-    while (--size){
-        *(b++) = *(a++);
-    }
-    return b;
-}
-
-void output(int *a, int size) {
-    int i;
-    for (i=0; i < size; i++){
-        printf("%d", *(a+i));
-    }
-    printf("\n");
-}
-
-int main()
-    {
-        int *a = createArray(20);
-        quicksort_2_way(a,0,19);
-        output(a,20);
-    }
