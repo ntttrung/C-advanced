c2d593da4e8e5b472a2c175d0129e2acab81acce
ntttrung <trung.ntt1210@gmail.com>
2020-09-24 13:50:46 +0700

    done

--- L1/quicksort.c
+++ L1/quicksort.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define MAX_NUMBER 1000
+#define MAX_NUMBER 100000000
 
 void exch(int a[], int i , int j);
 int* createArray(int n);
@@ -11,32 +11,15 @@ void output(int x[], int n);
 void quicksort_2_way(int a[], int l, int r) ;
 void quicksort_3_way(int a[], int l, int r) ;
 
-int main()
-{
-	time_t start, end;
-	int *a1,*a2;
-	srand(time NULL);
-	a1=createArray(MAX_NUMBER);
-	a2= dumpArray(a1, MAX_NUMBER);
-	
-	printf("Number of integer:%d\n", MAX_NUMBER);
-	start= clock();
-    quicksort_2_way(a1, 0 , MAX_NUMBER -1);
-	end = clock();
-	//output(a1,MAX_NUMBER);
-	printf("Run in %.8f second(s)\n", difftime(end, start)/CLOCKS_PER_SEC);
 
-	start= clock();
-    quicksort_3_way(a1, 0 , MAX_NUMBER -1);
-	end = clock();
-	//output(a2,MAX_NUMBER);
-	printf("Run in %.8f second(s)", difftime(end, start)/CLOCKS_PER_SEC);
-
-	
-
-
-
-}
+int int_compare(void const* x, void const* y)
+	{
+		int m,n;
+		m = *((int*)x);
+		n = *((int*)y);
+		if(m==n) return 0;
+		return m > n ? 1: -1;
+	}
 
 int* createArray(int n)
 {
@@ -107,4 +90,36 @@ void quicksort_3_way(int a[], int l, int r) {
     }
     quicksort_3_way(a, l, p - 1);
     quicksort_3_way(a, q + 1, r);
+}
+
+
+int main()
+{
+	time_t start, end;
+	int *a1,*a2,*a3;
+	srand(time NULL);
+	a1=createArray(MAX_NUMBER);
+	a2= dumpArray(a1, MAX_NUMBER);
+	a3 = dumpArray(a1,MAX_NUMBER);
+	printf("Number of integer:%d\n", MAX_NUMBER);
+	start= clock();
+    quicksort_2_way(a1, 0 , MAX_NUMBER -1);
+	end = clock();
+	//output(a1,MAX_NUMBER);
+	printf("Run in %.8f second(s)\n", difftime(end, start)/CLOCKS_PER_SEC);
+
+	start= clock();
+    quicksort_3_way(a1, 0 , MAX_NUMBER -1);
+	end = clock();
+	//output(a2,MAX_NUMBER);
+	printf("Run in %.8f second(s)\n", difftime(end, start)/CLOCKS_PER_SEC);
+
+	start= clock();
+	qsort(a3,MAX_NUMBER, sizeof(int),int_compare);
+	end = clock();
+	//output(a1,MAX_NUMBER);
+	printf("Run in %.8f second(s)\n", difftime(end, start)/CLOCKS_PER_SEC);
+	free(a1);
+	free(a2);
+	free(a3);
 }
