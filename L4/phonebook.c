99ca78269add0892740d49a95d7c9f253fe025d9
ntttrung <trung.ntt1210@gmail.com>
2020-11-05 13:31:22 +0700

    done

--- L4/phonebook.c
+++ L4/phonebook.c
@@ -1,12 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"jrb.c"
+#include"jrb.h"
 #include"jval.h"
+
+void jrb_print_db(JRB tree)
+    {
+        JRB tmp;
+        jrb_traverse(tmp, tree)
+            printf("Name: %s - Phone: %ld\n", jval_s(tmp->key), jval_l(tmp->val));
+    }
+
+JRB jrb_find_num(JRB tree, Jval value)
+    {
+        JRB tmp;
+        jrb_traverse(tmp, tree)
+            {
+                if(jval_l(tmp->key) == jval_l(value))
+                    return tmp;
+            }
+        return NULL;
+    }
 
 int main()
     {
-        JRB phonebook, node;
+        JRB phonebook, node,s;
         phonebook = make_jrb();
 
         jrb_insert_str(phonebook, "Chung", new_jval_l(99912335));
@@ -17,5 +35,8 @@ int main()
         jrb_insert_str(phonebook, "Tung", new_jval_l(979327));
         jrb_insert_str(phonebook, "Thang", new_jval_l(973497));
 
-        jrb_print_tree(phonebook, 1);
+        jrb_print_db(phonebook);
+
+        s = jrb_find_num(phonebook, new_jval_l(913997) );
+        printf("%s\n", s->key);
     }
